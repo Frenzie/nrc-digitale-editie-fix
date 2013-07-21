6952c224a6dd8d198584464dda0c6cca3b2a5624
Frans de Jonge <fransdejonge@gmail.com>
2013-07-21 13:03:18 +0200

    Initial commit of 2.0

---
+++ nrc-digitale-editie-fix-settings.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name		NRC Digitale Editie Fix Settings
+// @author 		Frenzie
+// @include		http://digitaleeditie.nrc.nl/*
+// ==/UserScript==
+
+window.opera.UJSFixNRCSettings = {
+		// Whether to open the article text instead of the picture by default.
+		// true to enable
+		textDefault : true,
+}
--- nrc-digitale-editie-fix.js
+++ nrc-digitale-editie-fix.js
@@ -1,73 +1,103 @@
 // ==UserScript==
 // @name		NRC Digitale Editie Fix
 // @author 		Frenzie
-// @version		1.01
+// @version		2.0
 // @namespace		http://extendopera.org/userjs/content/nrc-digitale-editie-fix
-// @description		Suppresses the default annoying behavior of digital NRC. Instead of opening articles in a popup window only accessible through left click, this script replaces this ludicrousness with regular links so you get all the power that comes with regular links, such as opening them in background, creating linked windows, etcetera.
-// @include		http://www.nrc.nl/digitaleeditie/*
+// @description		Suppresses the default annoying behavior of digital NRC. Instead of opening articles in a lightbox only accessible through left click, this script replaces this ludicrousness with regular links so you get all the power that comes with regular links, such as opening them in background, creating linked windows, etcetera.
+// @include		http://digitaleeditie.nrc.nl/*
 // ==/UserScript==
 // Changelog
-// 1.0 July 3, 2010. Initial release.
+// 2.0 January 30, 2011. Updated for new NRC site.
 // 1.01 July 5, 2010. Added suppression of the opening of search results in a new window.
+// 1.0 July 3, 2010. Initial release.
 
-if ( (location.hostname.indexOf('nrc.nl') != -1) && (location.pathname.indexOf('digitaleeditie') != -1) && (location.pathname.indexOf('.html') == -1) )
+if ( (location.hostname.indexOf('digitaleeditie.nrc.nl') != -1) )
 {
-	function articleLink(element) {
-		// Grabbed from http://www.nrc.nl/digitaleeditie/script2.js
-		var mode = document.location.href.match(/#([^#]+)$/)
-		if (element.className.match(/^ad/)) {
-			var ad = element.className.match(/^\S+/)
-			var uri = ad + '.html'
-		} else {
-			var article = element.className.match(/article\d+/)
-			if (mode) {
-				if (mode[1] == 'text') {
-					var uri = article + '.html#text'
-				} else if (mode[1] == 'original') {
-					var uri = article + '_image.html#original'
-				} else {
-					var name = 'NRCM_'+ document.location.href.split('/').slice(-3,-1).join('.').replace(/[_]+./,'.').replace(/\./g,'_')
-					var uri = name + '_' + article + '.pdf'
-				}
-			} else {
-				var uri = article + '_image.html'
+
+	// Blatant copy of the MyOpera Enhancements settings system.
+	var defaultScriptSettings = {
+
+/********************************/
+/**** Begin editable section ****/
+/********************************/
+
+		//*** Toggle core functions
+		// Whether to open the article text instead of the picture by default.
+		// true to enable
+		textDefault : false,
+
+/******************************/
+/**** End editable section ****/
+/******************************/
+	}
+
+	/*
+	 * Copy settings to variables for easier use later.
+	 * Check window.UJSMyOperaCommunityFESettings for backwards compatibility.
+	 */
+	var userSets = opera.UJSFixNRCSettings||defaultScriptSettings;
+
+	var textDefault          = !(userSets.textDefault          === undefined)? userSets.textDefault          :defaultScriptSettings.textDefault;
+
+
+	window.opera.addEventListener('BeforeScript', function (e)
+	{
+		// jQuery makes things simpler my ass.
+		if (e.element.src.indexOf('one-page-view.js') != -1)
+			{
+				e.element.text = e.element.text.replace(
+					'articleDivs.click(function(){ NrcOnePageView.Articles.showArticle(this);return false; })',
+					'articleDivs.each(function(){jQuery(this).html(\'<a href="\'+getArticleHref(this)+\'" style="display:block;width:100%;height:100%"></a>\')})'
+				);
+				/*
+				e.element.text = e.element.text.replace(
+					'areas.click(function(){ NrcOnePageView.Articles.showArticle(this);return false; });',
+					''
+				);
+				*/
+				e.element.text = e.element.text.replace(
+					'NrcOnePageView.Articles.articleHyperlinks.click(function(){ NrcOnePageView.Articles.showArticle(this);return false; })',
+					'NrcOnePageView.Articles.articleHyperlinks.each(function(){jQuery(this).attr(\'href\',getArticleHref(this))})'
+				);
+			}
+	}, false);
+
+	// Mostly just taken from the NRC website. The crux of the problem is that this is how links are added, rather than server-side.
+	// Read my lips: PROGRESSIVE ENHANCEMENT. Build a *working* website, then add some lousy Javascript special effects if you must.
+	// The whole idea behind this freaking UserJS is to rewrite the website into what it should've been like before any JS was added.
+	getArticleHref = function(el) {
+		//get url by using classname
+		var filename;
+		if (el.className && el.className.match(/^ad/)) { filename = el.className.match(/^\S+/) + ".html"; } 
+		else {
+			// Note textDefault which loads the text version of an article if true.
+			if ( (el.href && el.href.indexOf("#text") != -1) || textDefault ) {
+				filename = el.className.match(/article\d+/) + "_text.html";
+			}
+			else {
+				filename = el.className.match(/article\d+/) + "_image.html";
 			}
 		}
-		// End grabbed.
-		return uri;
+		return filename;
 	}
-	document.addEventListener('DOMContentLoaded',function() {
-		// The image map.
-		var articles = document.getElementsByTagName('area');
-		for (var i=0;i<articles.length;i++) {
-			var element = articles[i];
-			element.href = articleLink(element);
-		}
-		// The divs overlaid on the image map.
-		articles = document.getElementsByClassName('page')[0].childNodes;
-		for (var i=0;i<articles.length;i++) {
-			var element = articles[i];
-			if (element == '[object HTMLDivElement]')
-				element.outerHTML = '<a href="' + articleLink(element) + '">' + element.outerHTML + '</a>';
-		}
-		// The links on the right side of the page.
-		articles = document.getElementsByClassName('teaser_contents')[0].childNodes;
-		for (var i=0;i<articles.length;i++) {
-			var element = articles[i];
-			if (element == '[object HTMLDivElement]')
-				element.outerHTML = '<a href="' + articleLink(element) + '">' + element.outerHTML + '</a>';
-		}
-	}, false);
-	// Suppress default behavior on the articles.
-	window.opera.addEventListener('BeforeEventListener.click', function (e)
-	{
-		//opera.postError(e.event.currentTarget.parentNode.toString())
-		if (
-			e.event.currentTarget.parentNode == '[object HTMLMapElement]' // The image map, obviously.
-			|| e.event.currentTarget.parentNode == '[object HTMLHeadingElement]' // Search results.
-			|| e.event.currentTarget.parentNode.toString().indexOf('nrc.nl') != -1 // Links added to DIVs.
-		) {
-			e.preventDefault();
-		}
+
+	// Add some styles that are specific to this script.
+	var addStyle = function() {
+		var style = document.createElement('style');
+		style.setAttribute('type', 'text/css');
+		style.textContent = '\
+			#CloseButton {display:none !important}\
+			.column-left {float:none !important}\
+			.column-right {padding-left: 1em !important}\
+		';
+		document.body.previousSibling.appendChild(style);
+	}
+	document.addEventListener('DOMContentLoaded', addStyle, false);
+	
+	// Switches the left and right column in the DOM for easier styling.
+	document.addEventListener('DOMContentLoaded', function(){
+		var columnLeftTemp = jQuery('.column-left').clone();
+		jQuery('.column-left').remove();
+		columnLeftTemp.appendTo('.columns');
 	}, false);
 }
